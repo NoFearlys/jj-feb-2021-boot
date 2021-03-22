85fc97fbdae7c548841cd7e82b2fbb6c828bc572
NoFearlys <trofimov_a_a@mail.ru>
2021-03-22 16:06:16 +0300

    authenticate add

--- src/main/java/com/lesson3/jjfeb2021/web/SecurityConfig.java
+++ src/main/java/com/lesson3/jjfeb2021/web/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.lesson3.jjfeb2021.web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 
 @Configuration
@@ -16,8 +21,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf();
 
         http.authorizeRequests()
-                .antMatchers("/", "/client/*").permitAll()
+                .antMatchers("/**", "/client/*").authenticated()
                 .antMatchers(HttpMethod.GET, "/**/*.css", "/**/*.js", "/**/*.scss", "/**/*.ico").permitAll()
-                .anyRequest().denyAll();
+                .anyRequest().denyAll()
+        .and()
+                .formLogin()
+        .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    public UserDetailsService user(){
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{bcrypt}$2y$12$vorCQV44fteaPYVzEeXE4.MfeasNH0UjEarymGUyQEn480FoOc.Va")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
     }
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,13 +1,9 @@
 var elements = $('.modal-overlay, .modal');
 var modalClientFields = document.getElementsByClassName('modalClient')
 var modal = document.getElementById('clientModal')
-var btn = document.getElementById('btnModal')
 var content = document.getElementById('content')
 
 
-btn.onclick = function (){
-    elements.addClass('active');
-}
 
 window.onclick = function (event){
     if(event.target == modal){
